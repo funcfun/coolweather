37a063dcb9f72de207d3ff27bc63deb43dd724bf
mooner <zichenzhiguang@126.com>
2015-11-25 15:11:10 +0800

    加入后台自动更新天气的功能（未调试）。

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -15,7 +15,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
-import android.util.Log;
 
 public class Utility {
 
